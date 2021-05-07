49cbfabdee7a2de42ff4a61e4426a8f9e4d65e01
Gwoat <77695380+Gwoat@users.noreply.github.com>
2021-05-07 12:41:00 -0700

    Update tkinterTest.py

--- tkinterTest.py
+++ tkinterTest.py
@@ -5,33 +5,62 @@ songList = []
 
 def addTrack():
     songList.append(E1.get())
+    E1.delete(0, END)
 
 
 def printList():
     print(songList)
 
-#this is a label widget
-L1 = Label(top, text="Playlist Generator")
+def exportList():
+    with open("test.txt", "w") as f:
+        for item in songList:
+            f.write("%s\n" % item)
 
-L1.grid(column= 0, row= 1)
+def clearWindow():
+    for widget in top.winfo_children():
+        widget.destroy()
+        
+def mainMenu():
+    clearWindow()
+    LMmain = Label(top, text= "Block 5 GUI Projects")
+    Lmain.grid(column = 0, row= 1)
+    
+    B1Main = Button(text = "Week 1", bg = "pink", command = week1)
+    B1Main.grid(column = 0, row=1)
+    
+    B2Main = Button(text = "Week 2", bg = "purple", command = week2)
+    B2Main.grid(column = 1, row 2)
+    
+    B3Main = Button(text = "Week 3", bg = "orange", command = week3)
+    B3Main.grid(column = 1, row = 3)
 
-#This is an entry widget (for text entry)
-E1 = Entry(top, bd = 5)
+def week1():
+    clearWindow
+    #this is a label widget
+    L1 = Label(top, text="LETS GO")
 
-E1.grid(column= 0, row = 2)
-#This is a button widget.
-B1 = Button(text = "Add to playlist", bg = "pink", command = addTrack)
+    L1.grid(column= 0, row= 1)
 
-B1.grid(column= 2, row = 1)
+    #This is an entry widget (for text entry)
+    E1 = Entry(top, bd = 5)
 
+    E1.grid(column= 0, row = 2)
+    #This is a button widget.
+    B1 = Button(text = "+", bg = "pink", command = addTrack)
 
-
-
-#print button
-B2 = Button(text = "Print", bg = "orange", command = printList)
-
-B2.grid(column= 2, row = 2)
-
+    B1.grid(column= 2, row = 1)
 
 
-top.mainloop()
+
+
+    #print button
+    B2 = Button(text = "Print", bg = "orange", command = printList)
+
+    B2.grid(column= 2, row = 2)
+
+    #export button
+    B3 = Button(text = "Exporte", bg = "purple", command = exportList)
+
+    B3.grid(column= 3, row = 2)
+
+
