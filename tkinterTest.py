be9d1cb8e37b859d8369ada627dde7133508980a
Gwoat <77695380+Gwoat@users.noreply.github.com>
2021-05-14 12:30:30 -0700

    Update tkinterTest.py

--- tkinterTest.py
+++ tkinterTest.py
@@ -2,7 +2,8 @@ from tkinter import *
 
 top = Tk()
 songList = []
-
+rollTimes = 0
+dieType = 0
 
 
 def printList():
@@ -23,13 +24,13 @@ def mainMenu():
     LMain.grid(column = 0, row= 1)
     
     B1Main = Button(text = "Week 1", bg = "pink", command = week1)
-    B1Main.grid(column = 0, row=1)
+    B1Main.grid(column = 1, row=2)
     
     B2Main = Button(text = "Week 2", bg = "purple", command = week2)
-    B2Main.grid(column = 1, row = 2)
+    B2Main.grid(column = 1, row = 3)
     
     B3Main = Button(text = "Week 3", bg = "orange", command = week3)
-    B3Main.grid(column = 1, row = 3)
+    B3Main.grid(column = 1, row = 4)
 
 def week1():
 
@@ -66,9 +67,40 @@ def week1():
     B3 = Button(text = "Exporte", bg = "purple", command = exportList)
 
     B3.grid(column= 3, row = 2)
+
+    B4 = Button(text="Main Menu", bg = "yellow", command = mainMenu)
+
+    B4.grid(column = 0, row = 3)
     
 
 def week2():
+    def rollDice():
+        #update variable data
+        
+        rollTimes = E2W2.get()
+        dieType = E1W2.get()
+
+        #clear the window
+
+        clearWindow()
+
+        #calculate the dice rolls
+
+        for x in range(0, int(rollTimes)):
+            myRolls.append(random.randint(1, int(dieType)))
+
+        #display the rolls and an exit button
+        L4W2 = Label(top, text= "Here are your rolls!" )
+        L4W2.grid(column= 0, row = 1)
+
+        L5W2 = Label(top, text= "{}".format(myRolls))
+        L5W2.grid(column= 0, row = 2)                             
+
+                               
+        B2W2 = Button(text = "Main Menu", bg= "yellow", command = mainMenu)
+        B2W2.grid(column = 0, row = 3)
+
+    
     clearWindow()
 
     L1W2 = Label(top, text= "Dice Roller App")
@@ -78,7 +110,7 @@ def week2():
     L2W2 = Label(top, text= "# of sides")
     L2W2.grid(column = 0, row = 2)
     
-    L3W2 = Lable(top, text= "# of rolls")
+    L3W2 = Label(top, text= "# of rolls")
     L3W2.grid(column = 0, row = 3)
     
     E1W2 = Entry(top, bd = 5)
@@ -93,7 +125,8 @@ def week2():
     B1W2.grid(column = 2, row = 4)
 
 
-
+def week3():
+    pass
 
 
 
